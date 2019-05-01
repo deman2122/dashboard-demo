b9340ba01a3cf5a847d9e1e104e92ca2e31b7bb8
mehroosali <mehroosali@gmail.com>
2019-05-01 13:30:38 +0530

    chart setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react';
-import '.App.css';
+import React from 'react';
+import Chart1 from './Components/Chart1';
+import Chart2 from './Components/Chart2';
+import Chart3 from './Components/Chart3';
 
-class App extends Component {
+class App extends React.Component {
+  state = {};
+
+  componentDidMount() {}
+
   render() {
     return (
-      <div>
-        hey!
+      <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-md-6'>
+            <Chart1 />
+          </div>
+          <div className='col-md-6'>
+            <Chart2 />
+          </div>
+
+          <div className='col-md-12'>
+            <Chart3 />
+          </div>
         </div>
-    )
+      </div>
+    );
   }
 }
 
